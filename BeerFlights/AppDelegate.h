e956ad18452ae3077092be4c496d838cd95bdb9f
Julie Yaunches <jmyaunch@gmail.com>
2013-10-31 17:30:49 -0400

    Initial Commit

---
+++ BeerFlights/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BeerFlights
+//
+//  Created by Julietta Yaunches on 31/10/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeerFlights/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BeerFlights
+//
+//  Created by Julietta Yaunches on 31/10/13.
+//  Copyright (c) 2013 Julietta Yaunches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
